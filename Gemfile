b570460016c6f58d5e970b399ec036a8765b07bc
John Mazzucco <John.Mazzucco@gmail.com>
2015-05-13 18:16:14 -0400

    Installed Favorite things,changed to source in User model,took out reset password from sorcery.rb

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'sorcery'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'favorite_things'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   has_many :pledges
   has_many :owned_projects, class_name: 'Project'
-  has_many :backed_projects, through: :pledges, class_name: 'Project'
+  has_many :backed_projects, through: :pledges, source: :project
 end
