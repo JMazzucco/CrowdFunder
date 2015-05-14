ed8e5b7c71dfd52656ee0b14e0abf4045b5d5b18
John Mazzucco <John.Mazzucco@gmail.com>
2015-05-13 22:44:42 -0400

    Added Index, Add and Create actions in the Products controller and RESTful routes for Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,25 @@
 class ProductsController < ApplicationController
   def index
+ 		@projects = Project.all
   end
 
   def new
+  	@product = Project.new
   end
 
   def create
+  	@product = Project.new(project_params)
+
+  	if @product.save
+  		redirect_to project_url
+  	else
+  		render :new
+  	end
   end
+
+  private
+  def project_params
+  		params.require(:project).permit(:name, :description, :funding_goal, :start_date, :end_date, :photo)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
-  get 'products/new'
-
-  get 'products/create'
+  resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
