990ba7575e79fca6467c56953f5040a71ecbbfa5
Mary Viets <mary.evelyn.viets@gmail.com>
2015-05-14 23:29:35 -0400

    added validations on the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :password, length: {minimum: 3}
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: true
+
   has_many :pledges
   has_many :owned_projects, class_name: 'Project'
   has_many :backed_projects, through: :pledges, source: :project
