e10414c782ac59b715ddb3390fe7ca7c974ed1da
Harrison Taylor <h.taylor@project89.org>
2015-05-15 14:00:27 -0400

    Crud functionality for projects hath been initialized

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,15 +3,32 @@ class ProjectsController < ApplicationController
  		@projects = Project.all
   end
 
+  def show
+    @project = Project.find(params[:id])
+  end
+
   def new
   	@project = Project.new
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
   end
 
   def create
   	@project = Project.new(project_params)
 
   	if @project.save
-  		redirect_to project_url
+  		redirect_to projects_url
   	else
   		render :new
   	end
